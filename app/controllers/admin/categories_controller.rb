f256779d4196e22209efa0916030a4b71ff79639
Jen Caza <jencaza@gmail.com>
2022-02-07 20:59:19 -0500

    correct typo

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,40 +1,30 @@
-<section class="admin-categories-new">
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+  
+  def index
+    @categories = Category.all
+  end
 
-  <header class="page-header">
-    <h1>Admin &raquo; New Category</h1>
-  </header>
+  def new
+    @category = Category.new
+  end
 
-  <div class="well">
-    <%= link_to 'All Categories', [:admin, :categories], class: 'btn btn-info' %>
-  </div>
+  def create
+    @category = Category.new(category_params)
 
-  <div class="row">
-    <div class="col-sm-6 col-sm-offset-1">
-      <%= form_for [:admin, @category] do |form| %>
-        <% if form.object.errors.any? %>
-          <div class="alert alert-danger">
-            <strong>The following errors prevented saving:</strong>
-            <ul>
-              <% form.object.errors.full_messages.each do |error| %>
-                <li><%= error %></li>
-              <% end %>
-            </ul>
-          </div>
-        <% end %>
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 
-        <div class="form-group">
-          <%= form.label :name %>
-          <%= form.text_field :name, class: 'form-control' %>
-        </div>
+  private
 
-        <div class="form-group">
-          <%= form.label :quantity %>
-          <%= form.number_field :quantity, class: 'form-control' %>
-        </div>
-        
-        <%= form.submit 'Create Category', class: 'btn btn-primary' %>
-      <% end %>
-    </div>
-  </div>
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 
-</section>
+end
